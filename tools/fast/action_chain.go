75ae1f8d9d1f755deae0a8ce5ba1ee4234bc42c9
frrist <forrest@protocol.ai>
2019-09-24 09:42:37 +1000

    add FAST actions for import and export

--- tools/fast/action_chain.go
+++ tools/fast/action_chain.go
@@ -3,8 +3,10 @@ package fast
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-ipfs-files"
 
 	"github.com/filecoin-project/go-filecoin/chain"
 )
@@ -31,4 +33,28 @@ func (f *Filecoin) ChainStatus(ctx context.Context) (*chain.Status, error) {
 		return nil, err
 	}
 	return out, nil
+}
+
+// ChainExport runs the chain export command against the filecoin process.
+func (f *Filecoin) ChainExport(ctx context.Context, filepath string) error {
+	out, err := f.RunCmdWithStdin(ctx, nil, "go-filecoin", "chain", "export", filepath)
+	if err != nil {
+		return err
+	}
+	if out.ExitCode() > 0 {
+		return fmt.Errorf("filecoin command: %s, exited with non-zero exitcode: %d", out.Args(), out.ExitCode())
+	}
+	return nil
+}
+
+// ChainImport runs the chain import command against the filecoin process.
+func (f *Filecoin) ChainImport(ctx context.Context, file files.File) error {
+	out, err := f.RunCmdWithStdin(ctx, file, "go-filecoin", "chain", "import")
+	if err != nil {
+		return err
+	}
+	if out.ExitCode() > 0 {
+		return fmt.Errorf("filecoin command: %s, exited with non-zero exitcode: %d", out.Args(), out.ExitCode())
+	}
+	return nil
 }
